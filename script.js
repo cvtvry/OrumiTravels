3f4f93fb82c49349c0407d0dba852c64ad22102e
Catalina Ramírez <blackandromeda.t@gmail.com>
2026-05-23 18:52:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
-const menuToggle = document.getElementById("menu-toggle");
+/* ================= LOADER ================= */
 
-const navLinks = document.getElementById("nav-links");
+window.addEventListener("load",()=>{
 
-menuToggle.addEventListener("click", () => {
+  setTimeout(()=>{
 
-    navLinks.classList.toggle("active");
+    document
+      .getElementById("loader")
+      .classList.add("hidden");
+
+  },1600);
 
 });
+
+/* ================= MENU ================= */
+
+document
+  .getElementById("toggle")
+  .onclick=()=>{
+
+    document
+      .getElementById("menu")
+      .classList.toggle("active");
+
+};
+
+/* ================= PARTICLES ================= */
+
+const particles=document.getElementById("particles");
+
+for(let i=0;i<45;i++){
+
+  let s=document.createElement("span");
+
+  s.style.left=Math.random()*100+"%";
+
+  s.style.animationDuration=
+    8+Math.random()*12+"s";
+
+  particles.appendChild(s);
+}
+
+/* ================= LEAVES ================= */
+
+const leaves=document.getElementById("leaves");
+
+for(let i=0;i<22;i++){
+
+  let l=document.createElement("span");
+
+  l.style.left=Math.random()*100+"%";
+
+  l.style.animationDuration=
+    10+Math.random()*12+"s";
+
+  leaves.appendChild(l);
+}
